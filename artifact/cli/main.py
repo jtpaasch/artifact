2e5f011119bc2d81f526f086fa944feb90efaa9b
JT Paasch <jt.paasch@gmail.com>
2015-10-17 01:55:06 -0400

    Adding some rudimentary cloud formation stuff.

--- artifact/cli/main.py
+++ artifact/cli/main.py
@@ -7,13 +7,215 @@ import click
 from botocore.exceptions import ClientError
 
 from artifact.client import autoscalinggroup
+from artifact.client import cloudformation
 from artifact.client import elasticloadbalancer
+from artifact.client import launchconfiguration
+
+
+def create_cf_template(username, password, email):
+    """Create a Cloud Formation template."""
+    template = []
+    template.append('{')
+    template.append('  "AWSTemplateFormatVersion": "2010-09-09",')
+    template.append('  "Parameters": {')
+    template.append('    "ContainerNameParameter": {')
+    template.append('      "Type": "String",')
+    template.append('      "Description": "Enter a name for the running container"')
+    template.append('    },')
+    template.append('    "ImageParameter": {')
+    template.append('      "Type": "String",')
+    template.append('      "Description": "Enter a registry image (and tag)"')
+    template.append('    },')
+    template.append('    "MinSizeParameter": {')
+    template.append('      "Type": "Number",')
+    template.append('      "Default": "1",')
+    template.append('      "Description": "Enter the min size"')
+    template.append('    },')
+    template.append('    "MaxSizeParameter": {')
+    template.append('      "Type": "Number",')
+    template.append('      "Default": "1",')
+    template.append('      "Description": "Enter the max size"')
+    template.append('    }')
+    template.append('  },')
+    template.append('  "Resources": {')
+    template.append('    "ReeferLaunchConfig": {')
+    template.append('      "Type": "AWS::AutoScaling::LaunchConfiguration",')
+    template.append('      "Properties": {')
+    template.append('        "AssociatePublicIpAddress": "true",')
+    template.append('        "ImageId": "ami-e3106686",')
+    template.append('        "InstanceType": "t2.micro",')
+    template.append('        "KeyName": "quickly_fitchet",')
+    template.append('        "SecurityGroups": [')
+    template.append('          "sg-be4db7d8"')
+    template.append('        ],')
+    template.append('        "UserData": {')
+    template.append('          "Fn::Base64": {')
+    template.append('            "Fn::Join": [')
+    template.append('              "",')
+    template.append('              [')
+    template.append('                "#!/bin/bash -xe\\n",')
+    template.append('                "\\n",')
+    template.append('                "# Install and start docker.\\n",')
+    template.append('                "yum install -y docker\\n",')
+    template.append('                "service docker start\\n",')
+    template.append('                "chkconfig docker on\\n",')
+    template.append('                "\\n",')
+    if username and password and email:
+        template.append('                "# Authenticate with docker hub.\\n",')
+        template.append('                "docker login \\\\n",')
+        template.append('                "    -u ' + username + ' \\\\n",')
+        template.append('                "    -p ' + password + ' \\\\n",')
+        template.append('                "    -e ' + email + '\\n",')
+        template.append('                "\\n",')
+    template.append('                "# Pull the docker image.\\n",')
+    template.append('                "docker pull ", {"Ref": "ImageParameter"}, " \\n",')
+    template.append('                "\\n",')
+    template.append('                "# Start the container.\\n",')
+    template.append('                "docker run",')
+    template.append('                "    --name ", {"Ref": "ContainerNameParameter"},')
+    template.append('                "    --restart always",')
+    template.append('                "    -dti",')
+    template.append('                "    -p 80:80",')
+    template.append('                "    ", {"Ref": "ImageParameter"}, "\\n",')
+    template.append('                "\\n"')
+    template.append('              ]')
+    template.append('            ]')
+    template.append('          }')
+    template.append('        }')
+    template.append('      }')
+    template.append('    },')
+    template.append('    "ReeferASG": {')
+    template.append('      "Type": "AWS::AutoScaling::AutoScalingGroup",')
+    template.append('      "Properties": {')
+    template.append('        "LaunchConfigurationName": {"Ref": "ReeferLaunchConfig"},')
+    template.append('        "MinSize": "1",')
+    template.append('        "MaxSize": "1",')
+    template.append('        "VPCZoneIdentifier": [')
+    template.append('          "subnet-a1e1f9d6",')
+    template.append('          "subnet-33d2e06a"')
+    template.append('        ]')
+    template.append('      }')
+    template.append('    }')
+    template.append('  }')
+    template.append('}')
+    return "\n".join(template)
+
+
+def create_user_data(image, username, password, email):
+    """Create a user_data script that pulls and runs a docker container."""
+    user_data = []
+    user_data.append("#!/bin/bash")
+    user_data.append("")
+    user_data.append("# Print out debug info and exit on any failure.")
+    user_data.append("set -x -e")
+    user_data.append("")
+    user_data.append("# Install and start docker.")
+    user_data.append("yum install -y docker")
+    user_data.append("service docker start")
+    user_data.append("chkconfig docker on")
+    user_data.append("")
+    if username and password and email:
+        user_data.append("# Authenticate with docker.")
+        user_data.append("docker login \\")
+        user_data.append("    -u '" + username + "' \\")
+        user_data.append("    -p '" + password + "' \\")
+        user_data.append("    -e '" + email + "'")
+        user_data.append("")
+    ps_name = image.replace("/", "_").replace(":", "_")
+    user_data.append("# Pull down the docker image.")
+    user_data.append("docker pull " + image)
+    user_data.append("")
+    user_data.append("# Now run the container.")
+    user_data.append("docker run \\")
+    user_data.append("    --name " + ps_name + " \\")
+    user_data.append("    --restart always \\")
+    user_data.append("    -dti \\")
+    user_data.append("    -p 80:80 \\")
+    user_data.append("    " + image)
+    user_data.append("")
+    return "\n".join(user_data)
+
+
+def create_launch_configuration(name, image, username, password, email):
+    """Create a launch configuration for a docker image."""
+    user_data = create_user_data(image, username, password, email)
+    params = {}
+    params["name"] = name
+    params["ami_id"] = "ami-e3106686"
+    params["key_name"] = "quickly_fitchet"
+    params["security_groups"] = ["sg-be4db7d8"]
+    params["public_ip"] = True
+    params["user_data"] = user_data
+    response = launchconfiguration.create_launch_configuration(**params)
+    return response
 
 
 @click.group()
 def cli():
     """Create artifacts in the cloud."""
     pass
+
+
+@cli.group()
+def cf():
+    """Manage cloud formation artifacts."""
+    pass
+
+
+@cf.command(name="create")
+@click.argument("name")
+@click.option(
+    "--min-size",
+    default=1,
+    help="Minimum number of instances.")
+@click.option(
+    "--max-size",
+    default=1,
+    help="Maximum number of instances.")
+@click.option(
+    "--image",
+    help="Docker registry image (with tag).")
+@click.option(
+    "--username",
+    help="Docker registry username.")
+@click.option(
+    "--password",
+    help="Docker registry password.")
+@click.option(
+    "--email",
+    help="Docker registry email.")
+@click.option(
+    "--template-file",
+    help="Cloud Formation template file.")
+def create_cf(
+        name,
+        min_size,
+        max_size,
+        image,
+        username,
+        password,
+        email,
+        template_file):
+    """Create a Cloud Formation artifact."""
+    ps_name = image.replace("/", "_").replace(":", "_")
+    ps_param = {"ParameterKey": "ContainerNameParameter", "ParameterValue": ps_name}
+    image_param = {"ParameterKey": "ImageParameter", "ParameterValue": image}
+    parameters = [ps_param, image_param]
+    params = {}
+    params["name"] = name
+    params["parameters"] = parameters
+    if template_file:
+        params["template_file"] = template_file
+    else:
+        params["template"] = create_cf_template(username, password, email)
+    response = cloudformation.create_stack(**params)
+
+
+@cf.command(name="delete")
+@click.argument("name")
+def delete_cf(name):
+    """Delete a Cloud Formation artifact."""
+    response = cloudformation.delete_stack(name)
 
 
 @cli.group()
@@ -24,10 +226,6 @@ def app():
 
 @app.command(name="create")
 @click.argument("name")
-@click.option(
-    "--launch-configuration",
-    default="BasicLaunchConfig",
-    help="Used to launch new EC2 instances.")
 @click.option(
     "--min-size",
     default=1,
@@ -40,15 +238,41 @@ def app():
     "--desired-size",
     default=1,
     help="Desired number of instances.")
-def create_app(name, launch_configuration, min_size, max_size, desired_size):
+@click.option(
+    "--image",
+    help="Docker registry image (with tag).")
+@click.option(
+    "--username",
+    help="Docker registry username.")
+@click.option(
+    "--password",
+    help="Docker registry password.")
+@click.option(
+    "--email",
+    help="Docker registry email.")
+def create_app(
+        name,
+        min_size,
+        max_size,
+        desired_size,
+        image,
+        username,
+        password,
+        email):
     """Create an application artifact."""
+    create_launch_configuration(
+        name,
+        image,
+        username,
+        password,
+        email)
     params = {}
     params["name"] = name
-    params["launch_configuration"] = launch_configuration
     params["min_size"] = 1
     params["max_size"] = 1
     params["desired_size"] = 1
     params["subnets"] = ["subnet-f6e9cadd", "subnet-a1e1f9d6"]
+    params["launch_configuration"] = name
     try:
         response = autoscalinggroup.create_auto_scaling_group(**params)
     except ClientError as e:
@@ -78,14 +302,26 @@ def delete_app(name):
     except ClientError as e:
         message = "AutoScalingGroup name not found"
         if e.response["Error"]["Message"].startswith(message):
-            raise click.ClickException(name + " does not exist.")
+            raise click.ClickException("No auto scaling group called " + name)
     success = False
     metadata = response.get("ResponseMetadata")
     if metadata:
         status_code = metadata.get("HTTPStatusCode")
         if status_code == 200:
             success = True
-    if success:
+    try:
+        response = launchconfiguration.delete_launch_configuration(name)
+    except ClientError as e:
+        message = "Launch configuration name not found"
+        if e.response["Error"]["Message"].startswith(message):
+            raise click.ClickException("No launch config called " + name)
+    success_2 = False
+    metadata = response.get("ResponseMetadata")
+    if metadata:
+        status_code = metadata.get("HTTPStatusCode")
+        if status_code == 200:
+            success_2 = True
+    if success and success_2:
         click.echo(name + " terminating.")
     else:
         message = name + " was not deleted. Something went wrong."
---
+++ artifact/client/cloudformation.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+"""A module to handle Cloud Formation stacks."""
+
+from artifact.client.utils import get_client
+from artifact.client.utils import get_file_contents
+
+
+def create_stack(name, template=None, template_file=None, parameters=None):
+    """Create a Cloud Formation stack."""
+    client = get_client("cloudformation")
+    params = {}
+    params["StackName"] = name
+    if template:
+        params["TemplateBody"] = template
+    elif template_file:
+        params["TemplateBody"] = get_file_contents(template_file)
+    if parameters:
+        params["Parameters"] = parameters
+    response = client.create_stack(**params)
+    return response
+
+
+def get_stacks():
+    """Get info about all Cloud Formation stacks."""
+    client = get_client("cloudformation")
+    response = client.describe_stacks()
+    return response
+
+
+def delete_stack(name):
+    """Delete a Cloud Formations tack."""
+    client = get_client("cloudformation")
+    response = client.delete_stack(StackName=name)
+    return response
